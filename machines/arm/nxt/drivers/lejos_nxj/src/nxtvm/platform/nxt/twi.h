4e4a397ea06e88120a6c9bf6ac1198f663a28ac5
egrolleau <grolleau@ensma.fr>
2020-11-10 18:04:59 +0100

    Update twi.h

--- machines/arm/nxt/drivers/lejos_nxj/src/nxtvm/platform/nxt/twi.h
+++ machines/arm/nxt/drivers/lejos_nxj/src/nxtvm/platform/nxt/twi.h
@@ -5,8 +5,6 @@
 
 /* Main user interface */
 int twi_init(void);
-void twi_isr_C_function(void);
-
 void twi_start_write(U32 dev_addr, const U8 *data, U32 nBytes);
 void twi_start_read(U32 dev_addr, U8 *data, U32 nBytes);
 int twi_status(void);
