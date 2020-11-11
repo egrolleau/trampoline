308a6da6d1388f3ad140f0b6ea5ea2c0654992d3
egrolleau <grolleau@ensma.fr>
2020-11-11 10:19:45 +0100

    Update missing_symb.c

--- machines/arm/nxt/drivers/missing_symb.c
+++ machines/arm/nxt/drivers/missing_symb.c
@@ -3,33 +3,33 @@
 #include "buttons.h"
 
 FUNC(void, OS_CODE) systick_isr_C_function(void) {
-	return systick_isr_C();
+	systick_isr_C();
 }
 
 FUNC(void, OS_CODE) nxt_motor_isr_C_function(void) {
-	return nxt_motor_isr_C();
+	nxt_motor_isr_C();
 }
 
 FUNC(void, OS_CODE) spi_isr_C_function(void) {
-	return spi_isr_C();
+	spi_isr_C();
 }
 
 FUNC(void, OS_CODE) bt_isr_C_function(void) {
-	return bt_isr_C();
+	bt_isr_C();
 }
 
 FUNC(void, OS_CODE) twi_isr_C_function(void) {
-	return twi_isr_C();
+	twi_isr_C();
 }
 
 FUNC(void, OS_CODE) systick_low_priority_C_function(void) {
-	return systick_low_priority_C();
+	systick_low_priority_C();
 }
 
 FUNC(void, OS_CODE) i2c_timer_isr_C_function(void) {
-	return i2c_timer_isr_C();
+	i2c_timer_isr_C();
 }
 
 FUNC(void, OS_CODE) check_buttons_status_function(void) {
-	return check_buttons_status();
+	check_buttons_status();
 }
